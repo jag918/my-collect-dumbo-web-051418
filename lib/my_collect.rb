b18d01c01778a6101ed06ea16ec74d0305cce481
jee <jag918@nyu.edu>
2018-05-08 03:51:44 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,8 @@ def collect(collection)
   new_collection = []
   while i < collection.length
     new_value = yield collection[i]
-    puts(new_value)
+    new_collection << new_value
     i = i + 1
   end
-end
-
-collect(["hello", "hello2"]) do |name|
-  name+="2"
+  new_collection
 end
